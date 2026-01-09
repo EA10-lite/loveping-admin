40ddd39a11be545042443c32efa5d523c96e38c5
Emmanuel Anyigor <emanuelanyigor@gmail.com>
2026-01-09 09:18:05 +0100

    created a reusable sheet modal and used it to build note details modal

---
+++ src/components/main/DetailsModal.tsx
@@ -0,0 +1,57 @@
+import type { PropsWithChildren } from "react";
+import { Eye, X } from "lucide-react";
+import { Button } from "../ui/button"
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "../ui/sheet"
+
+type DetailsModalProps = PropsWithChildren<{
+    title: string;
+}>
+
+const DetailsModal = ({
+    title,
+    children
+}: DetailsModalProps) => {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button
+            className="bg-transparent hover:bg-[#143C2B] text-primary rounded-xs w-full justify-start"
+        >
+            <Eye className="text-primary" />
+            <span>View</span>
+        </Button>
+      </SheetTrigger>
+      <SheetContent className="min-w-[404px] bg-secondary border-[0.5px] border-primary/8 right-10 rounded-xl h-fit max-h-[90vh] overflow-auto top-1/2 -translate-y-1/2 p-0 [&>button.absolute]:hidden">
+        <SheetHeader className="border-b border-primary/10 text-white flex flex-row items-center justify-between">
+          <SheetTitle className="text-white">{title}</SheetTitle>
+
+          <SheetClose className="rounded-full text-primary bg-primary/8 size-6 flex items-center justify-center">
+            <X className="size-4" />
+          </SheetClose>
+        </SheetHeader>
+
+        {children}
+
+        <SheetFooter>
+            <SheetClose>
+                <Button
+                    className="rounded-full w-full h-12"
+                >
+                    <span>Close</span>
+                </Button>
+            </SheetClose>
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
+  )
+}
+
+export default DetailsModal;
--- src/pages/notes/_components/NoteDetails.tsx
+++ src/pages/notes/_components/NoteDetails.tsx
@@ -1,11 +1,87 @@
+import { DetailsModal, Text } from "../../../components";
 import type { Note } from "../../../utils/types";
+import { formatDateString } from "../../../utils/formatter";
+import { mergeClasses } from "../../../lib/mergeclasses";
 
+const NoteDetails = ({ note } : { note: Note }) => {
+  return (
+    <DetailsModal
+        title="Nudge Details"
+    >
+        <div className="p-4 space-y-6">
+            <div className="space-y-4">
+                <Text
+                    title="User Information"
+                    type="h4"
+                    className="text-primary font-medium"
+                />
 
-const NoteDetails = ({ note }: { note: Note }) => {
-    return (
-        <div className=""></div>
-    )
+                <div className="space-y-2.5">
+                    <Field label="Fullname" value={note.user.name} />
+                    <Field label="Email Address" value={note.user.email} />
+                    <Field label="User ID" value={`User ID: ${note.user._id}`} />
+                </div>
+            </div>
+            <div className="space-y-4">
+                <Text
+                    title="Action Details"
+                    type="h4"
+                    className="text-primary font-medium"
+                />
+
+                <div className="space-y-2.5">
+                    <Field
+                        label="Created At:"
+                        value={formatDateString(note.createdAt)}
+                        className="flex-row"
+                    />
+                    <Field
+                        label="Last Updated:"
+                        value={formatDateString(note.updatedAt)}
+                        className="flex-row"
+                    />
+                </div>
+            </div>
+            <div className="space-y-4">
+                <Text
+                    title="Content"
+                    type="h4"
+                    className="text-primary font-medium"
+                />
+
+                <Field value={note.content} />
+            </div>
+        </div>
+    </DetailsModal>
+  )
 }
 
+const Field = ({
+    label,
+    value,
+    className
+} : {
+    label?: string;
+    value: string,
+    className?: string
+}) => {
+    return (
+        <div className={mergeClasses(`flex flex-col gap-1.5`, className)}>
+            {label && (
+                <Text
+                    title={label}
+                    type="h4"
+                    className="text-white"
+                />
+            )}
+
+            <Text
+                title={value}
+                type="p"
+                className="text-grey"
+            />
+        </div>
+    )
+}
 
 export default NoteDetails;
