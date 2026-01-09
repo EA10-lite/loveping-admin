e52a9ddd26bd2b58cd37728edf8f75fe51f1fef1
Emmanuel Anyigor <emanuelanyigor@gmail.com>
2026-01-09 08:23:46 +0100

    creating feedback table

--- src/pages/feedback/page.tsx
+++ src/pages/feedback/page.tsx
@@ -1,16 +1,83 @@
-import { Text } from "../../components";
+import { ReusableTable, TableAction, Text } from "../../components";
 import { Button } from "../../components/ui/button";
 import { PiExport } from "react-icons/pi";
+import { feedbacks } from "../../data/feedback";
+import { type ColumnDef } from "@tanstack/react-table";
+import { type Feedback as FeedbackType } from "../../utils/types";
+import { formatDateString } from "../../utils/formatter";
+import FeedbackDetails from "./_components/FeedbackDetails";
+import { FaStar } from "react-icons/fa";
 
-
+const columns: ColumnDef<FeedbackType>[] = [
+    {
+        accessorKey: "user",
+        header: "User",
+        cell: ({ row }) => {
+            const user = row.original.user;
+            return (
+                <span className="text-sm text-white">{user.email}</span>
+            )
+        }
+    },
+    {
+        accessorKey: "type",
+        header: "Type",
+        cell: ({ row }) => (
+            <span className="text-white capitalize">{row.getValue("type")}</span>
+        )
+    },
+    {
+        accessorKey: "rating",
+        header: "Rating",
+        cell: ({ row }) => {
+            const rating = row.getValue("rating") as number;
+            return (
+                <span className="text-sm text-white flex items-center gap-0.5">
+                    {Array.from({ length: rating }).map((_i, index) => (
+                        <FaStar key={index} color="gold" />
+                    ))}
+                </span>
+            )
+        }
+    },
+    {
+        accessorKey: "message",
+        header: "Feedback Preview",
+        cell: ({ row }) => (
+            <span className="text-white line-clamp-1 max-w-[300px]">
+                {row.getValue("message")}
+            </span>
+        )
+    },
+    {
+        accessorKey: "createdAt",
+        header: "Sudmitted on",
+        cell: ({ row }) => (
+            <span className="text-white">
+                {formatDateString(row.getValue("createdAt"))}
+            </span>
+        )
+    },
+    {
+        id: "actions",
+        header: "Action",
+        cell: () => (
+            <div className="flex justify-end">
+                <TableAction
+                    View={<FeedbackDetails />}
+                />
+            </div>
+        )
+    }
+]
 
 const Feedback = () => {
     return (
         <div className="notes">
-             <div className="page-header">
+            <div className="page-header">
                 <div className="flex items-center justify-between">
                     <Text
-                        title="Feedback"
+                        title="Feedbacks"
                         type="h4"
                         className="text-lg"
                     />
@@ -24,8 +91,29 @@ const Feedback = () => {
                     </Button>
                 </div>
             </div>
+
+            <div className="page-body mt-6">
+                <ReusableTable
+                    data={feedbacks}
+                    columns={columns}
+                    searchKey="summary"
+                    filters={[
+                        {
+                            columnKey: "type",
+                            title: "Type",
+                            options: ["Bug", "Feature", "Improvement"].map(c => ({ label: c, value: c }))
+                        },
+                        {
+                            columnKey: "status",
+                            title: "Status",
+                            options: ["new", "resolved", "closed"].map(s => ({ label: s, value: s }))
+                        }
+                    ]}
+                />
+            </div>
         </div>
     )
 }
 
 export default Feedback;
+
